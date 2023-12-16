c02e8bc936baaf84981ca5c121bce142c330b761
MuhammedMagdyy <mohamedmody04@gmail.com>
2023-12-16 23:40:21 +0200

    test: Add unit tests for DroneRepository.
    This commit includes unit tests for the DroneRepository class, covering
    the following scenarios:
    1. `saveDroneTest`: Tests the successful saving
    of a Drone entity.
    2. `getFoundDroneTest`: Tests retrieving an existing
    Drone by its ID.
    3. `getNotFoundDroneTest`: Tests retrieving a
    non-existing Drone by an invalid ID.

---
+++ src/test/java/org/elmenus/drones/repository/DroneRepositoryTests.java
@@ -0,0 +1,53 @@
+package org.elmenus.drones.repository;
+
+import jakarta.transaction.Transactional;
+import org.elmenus.drones.model.entity.Drone;
+import org.elmenus.drones.model.entity.DroneModel;
+import org.elmenus.drones.model.entity.DroneState;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+@SpringBootTest
+public class DroneRepositoryTests {
+    private final DroneRepository droneRepository;
+
+    @Autowired
+    public DroneRepositoryTests(DroneRepository droneRepository) {
+        this.droneRepository = droneRepository;
+    }
+
+    @Test
+    @Transactional
+    void saveDroneTest() {
+        Drone drone = Drone.builder()
+                .serialNumber("ez")
+                .model(DroneModel.LIGHTWEIGHT)
+                .weightLimit(100)
+                .batteryCapacity(100)
+                .state(DroneState.IDLE)
+                .build();
+
+        droneRepository.save(drone);
+
+        Assertions.assertTrue(drone.getId() > 0);
+    }
+
+    @Test
+    void getFoundDroneTest() {
+        Optional<Drone> drone = droneRepository.findById(21L);
+
+        Assertions.assertTrue(drone.isPresent());
+        Assertions.assertEquals(21L, drone.get().getId());
+    }
+
+    @Test
+    void getNotFoundDroneTest() {
+        Optional<Drone> drone = droneRepository.findById(20L);
+
+        Assertions.assertFalse(drone.isPresent());
+    }
+}
