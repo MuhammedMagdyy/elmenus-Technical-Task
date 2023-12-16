593b2f7dc4574af6b92be6de4e7b91940730c90d
MuhammedMagdyy <mohamedmody04@gmail.com>
2023-12-16 21:58:00 +0200

    feat: add ValidationExceptionHandler for handling validation errors with message and array of errors.

---
+++ src/main/java/org/elmenus/drones/helpers/ValidationExceptionHandler.java
@@ -0,0 +1,34 @@
+package org.elmenus.drones.helpers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestControllerAdvice
+public class ValidationExceptionHandler {
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, Object> handleInvalidArgument(MethodArgumentNotValidException exception) {
+        List<Map<String, String>> errorsList = new ArrayList<>();
+
+        exception.getBindingResult().getFieldErrors().forEach(fieldError -> {
+            Map<String, String> error = new HashMap<>();
+            error.put("field", fieldError.getField());
+            error.put("message", fieldError.getDefaultMessage());
+            errorsList.add(error);
+        });
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("message", "Validation Error");
+        response.put("errors", errorsList);
+
+        return response;
+    }
+}
