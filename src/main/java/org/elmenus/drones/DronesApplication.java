bd47b30a75f9a0aa87c9eec4c25ce1964b132e99
MuhammedMagdyy <mohamedmody04@gmail.com>
2023-12-16 23:25:02 +0200

    feat: Integration with swagger for API documentation.

--- src/main/java/org/elmenus/drones/DronesApplication.java
+++ src/main/java/org/elmenus/drones/DronesApplication.java
@@ -1,10 +1,23 @@
 package org.elmenus.drones;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Drones Technical Task",
+                version = "1.0.0",
+                contact = @Contact(
+                        name = "Mohamed Magdy",
+                        email = "mohamedmagdy121@outlook.com"
+                )
+        )
+)
 @EnableScheduling
 public class DronesApplication {
 
